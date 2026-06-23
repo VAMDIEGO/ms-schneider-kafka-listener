a4dd26e2b1a3115bcdd5ddfee9465f5da9f9737d
Diego Valencia <diego.val.maya@gmail.com>
2026-06-23 16:28:47 -0600

    basic structure

---
+++ src/main/java/com/ms/schneider/MsSchneiderKafkaListenerApplication.java
@@ -0,0 +1,13 @@
+package com.ms.schneider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSchneiderKafkaListenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsSchneiderKafkaListenerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/ms/schneider/repositories/CustomerMongoRepository.java
@@ -0,0 +1,5 @@
+package com.ms.schneider.repositories;
+
+public class CustomerMongoRepository {
+
+}
